97fca6ba9eb106d2b672071576b80e31d298aa13
balamurugan1999v <31644214+balamurugan1999v@users.noreply.github.com>
2019-01-24 00:04:47 +0530

    Create binrnt.cpp

---
+++ binrnt.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<string.h>
+void main()
+{
+int n,i=0,j.a=0;
+char s[100];
+cin>>s;
+j=strlen(s);
+for(;i<j;i++)
+{
+if((s[i]!='1') && (s[i]!='0'))
+a=a+1;
+}
+if(a!=0)
+cout<<"no";
+else
+cout<<"yes";
+return 0;
+}
