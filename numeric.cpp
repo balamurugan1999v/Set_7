e139db2ab06a9898da8571b5f114948a73601656
balamurugan1999v <31644214+balamurugan1999v@users.noreply.github.com>
2019-01-25 20:37:29 +0530

    Create numeric.cpp

---
+++ numeric.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+int t,i,c=0;
+char a[100];
+cin.get(a,100);
+t=strlen(a);
+for(i=0;i<t;i++)
+if((a[i]>='0')&&(a[i]<='9')
+c++;
+cout<<c;
+getch();
+}
